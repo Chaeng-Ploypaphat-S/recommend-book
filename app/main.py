7b3f9a4fc273d830c95dbe2d707a13129aa20840
Ploypaphat Saltz <ploypaphat.saltz@gmail.com>
2025-11-18 15:50:21 -0800

    feature: add item-based recommendation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from typing import List, Optional
 from fastapi import FastAPI, Query
+from recommender import item_based_recommendation
 from resolver import random_items, random_genres_items
+import orjson
 
-app = FastAPI()
+def default_dumps(v):
+    return orjson.dumps(v).decode()
+
+app = FastAPI(json_dumps=default_dumps)
 
 @app.get("/")
 async def root():
@@ -14,7 +19,7 @@ async def all_movies():
 
 @app.get("/genres/{genre}")
 async def genre_movies(genre: str):
-    return {"message": random_genres_items(genre)}
+    return {"result": random_genres_items(genre)}
 
 @app.get("/user-based/")
 async def user_based(params: Optional[List[str]] = Query(None)):
@@ -22,4 +27,4 @@ async def user_based(params: Optional[List[str]] = Query(None)):
 
 @app.get("/item-based/{item_id}")
 async def item_based(item_id: str):
-    return {"message": f"item based: {item_id}"}
+    return {"result": item_based_recommendation(item_id)}
--- app/recommender.py
+++ app/recommender.py
@@ -14,12 +14,20 @@ try:
 except FileNotFoundError:
     print(f"Error: File not found at {data_fname}")
     RATINGS_DF_CACHE = pd.DataFrame()
+    
+    
+try:
+    MOVIES_DF_CACHE = pd.read_csv(item_fname)
+except FileNotFoundError:
+    print(f"Error: File not found at {data_fname}")
+    MOVIES_DF_CACHE = pd.DataFrame()
 
 def model_train():
     ratings_df = RATINGS_DF_CACHE
     ratings_df["userId"] = ratings_df["userId"].astype("category")
     ratings_df["movieId"] = ratings_df["movieId"].astype("category")
     
+    # create a sparse matrix of all the users/repos
     rating_matrix = coo_matrix(
         (
             ratings_df["rating"].astype(np.float32),
@@ -35,6 +43,34 @@ def model_train():
     als_model.fit(weight * rating_matrix)
     pickle.dump(als_model, open(saved_model_fname, "wb"))
     return als_model
+
+def calculate_item_based(item_id, items):
+    loaded_model = pickle.load(open(saved_model_fname, 'rb'))
+    recs = loaded_model.similar_items(itemid=int(item_id), N=11)
+    return [str(items[r]) for r in recs[0]]
+
+import numpy as np # <-- You may need to add this import at the top of your recommender.py file
+
+def item_based_recommendation(item_id):
+    ratings_df = RATINGS_DF_CACHE
+    ratings_df["userId"] = ratings_df["userId"].astype("category")
+    ratings_df["movieId"] = ratings_df["movieId"].astype("category")
+    movies_df = MOVIES_DF_CACHE
+    items = dict(enumerate(ratings_df["movieId"].cat.categories))
     
+    try:
+        parsed_id = ratings_df["movieId"].cat.categories.get_loc(int(item_id))
+        result = calculate_item_based(parsed_id, items)
+    except KeyError:
+        result = []
+        
+    result = [int(x) for x in result if x != item_id]
+
+    record_df = movies_df[movies_df["movieId"].isin(result)]
+    cleaned_df = record_df.replace([float('inf'), float('-inf')], np.nan)
+    final_df = cleaned_df.replace([np.nan], [None])
+    
+    return final_df.to_dict("records")
+
 if __name__ == "__main__":
     model = model_train()
